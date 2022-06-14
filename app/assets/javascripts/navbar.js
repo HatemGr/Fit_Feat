7b46cfb34f18326b28b46c8b58e52f0bb817cf75
Joff317 <j.freixanet@hotmail.com>
2022-06-14 09:34:49 +0200

    navbar style sheet and html erb

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,55 @@
+const menu = document.querySelector(".menu");
+
+const menuItems = Array.from(menu.querySelectorAll("a"));
+let activeItem = menu.querySelector("[aria-selected]");
+const indicator = document.createElement("span");
+
+indicator.classList.add("indicator");
+menu.appendChild(indicator);
+console.log(getTransform(activeItem));
+if (activeItem) {
+	indicator.style.setProperty("transform", getTransform(activeItem));
+}
+
+/**
+ *
+ * @param {{currentTarget: HTMLElement}} e
+ */
+
+function onItemClick(e) {
+	if (e.currentTarget === activeItem) {
+		return;
+	}
+
+	activeItem?.removeAttribute("aria-selected");
+	activeItem?.removeAttribute("style");
+
+	e.currentTarget.setAttribute(
+		"style",
+		"background-color: rgba(var(--purple), 0.05)"
+	);
+	e.currentTarget.setAttribute("aria-selected", "true");
+	indicator.animate([{ transform: getTransform(e.currentTarget) }], {
+		fill: "both",
+		duration: 600,
+		easing: "cubic-bezier(.48, 1.55, .28,1)",
+	});
+	activeItem = e.currentTarget;
+}
+
+/**
+ *
+ * @param {HTMLElement} element
+ * @return {string}
+ */
+function getTransform(element) {
+	const transform = {
+		y: element.offsetTop,
+		scaleY: element.offsetHeight / 100,
+	};
+	return `translateY(${transform.y}px) scaleY(${transform.scaleY})`;
+}
+
+menuItems.forEach((item) => {
+	item.addEventListener("click", onItemClick);
+});
