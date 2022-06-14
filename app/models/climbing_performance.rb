692fd1d6f0c38945fed2e8809373412aa1803a28
HatemGr <hatemgribi@gmail.com>
2022-06-14 12:59:06 +0200

    HG add perf calculation

--- app/models/climbing_performance.rb
+++ app/models/climbing_performance.rb
@@ -1,3 +1,40 @@
 class ClimbingPerformance < ApplicationRecord
   belongs_to :sport_user
+
+  after_validation :calc_perf_score
+
+
+  def calc_perf_score 
+    score = 0
+    case level 
+    when 5
+      score += 2
+    when 6
+      score += 4
+    when 7
+      score += 8
+    when 8
+      score += 10
+    end
+
+    case block_color
+    when "vert"
+      score += 2
+    when "bleu"
+      score += 4
+    when "rouge"
+      score += 6
+    when "violet"
+      score += 8
+    when "orange"
+      score += 10
+    end
+
+    score /= 2
+
+    SportUser.find(self.sport_user_id).update(perf_score: score)
+  end
+
+
+
 end
--- app/models/running_performance.rb
+++ app/models/running_performance.rb
@@ -2,4 +2,36 @@ class RunningPerformance < ApplicationRecord
   belongs_to :sport_user
 
   validates :distance, :speed, numericality: { only_integer: true , :greater_than_or_equal_to => 0}
+  after_validation :calc_perf_score
+
+  def calc_perf_score 
+    score = 0
+    if distance < 5
+      score +=2
+      elsif distance < 10
+        score +=4
+      elsif distance < 15
+        score +=6
+      elsif distance < 20
+        score +=8
+      else
+        score += 10
+    end
+
+    if speed < 5
+      score +=2
+      elsif speed < 7
+        score +=4
+      elsif speed < 9
+        score +=6
+      elsif speed < 12
+        score +=8
+      else
+        score += 10
+    end
+
+    score /= 2
+
+    SportUser.find(self.sport_user_id).update(perf_score: score)
+  end
 end
