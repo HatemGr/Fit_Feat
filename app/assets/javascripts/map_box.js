cf91fecb2c34acedaa9147fa30b4803e93a4774a
HatemGr <hatemgribi@gmail.com>
2022-06-14 10:05:06 +0200

    HG transfer map to user show and add distance

--- app/assets/javascripts/map_box.js
+++ app/assets/javascripts/map_box.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
 var map = new mapboxgl.Map({
   container: 'map', //Container ID
   style: 'mapbox://styles/mapbox/light-v10', //Map style
-  center: [2.2374527, 48.9141275], //Starting postition
+  center: [mapElement.dataset.userLng, mapElement.dataset.userLat], //Starting postition
   zoom: 13
 });
 
