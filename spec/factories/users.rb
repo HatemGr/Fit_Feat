97ef80900650b281c350ff356aa8bf4ff2c30f09
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-06-15 20:18:19 +0200

    LL redirect after sign up to user edit and adding cities select on edit info partial

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+    factory :user do
+        sequence(:email) { |n| "email+#{n}@thp.com"} 
+        password { "password"} 
+        first_name { "surname" }
+        last_name { "name" }
+    end
+end
+
