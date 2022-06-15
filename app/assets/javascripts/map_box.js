d79440de8bb9881586c15cbb1770e16ce43dfb76
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-06-15 22:13:28 +0200

    LL fixing bug heroku

--- app/assets/javascripts/map_box.js
+++ app/assets/javascripts/map_box.js
@@ -11,9 +11,9 @@ var map = new mapboxgl.Map({
 
 var markers = JSON.parse(mapElement.dataset.markers);
   markers.forEach(function(marker){
-  let el = document.createElement('div');
-  el.className = 'marker';
-    new mapboxgl.Marker(el)
+  let element = document.createElement('div');
+  element.className = 'marker';
+    new mapboxgl.Marker(element)
     .setLngLat([marker.lng,marker.lat])
     .setPopup(new mapboxgl.Popup({offset:25})
       .setHTML(
