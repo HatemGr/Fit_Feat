3aa554bf17c992b9313aeb99f9f4c9cc19163eae
HatemGr <hatemgribi@gmail.com>
2022-06-16 23:17:47 +0200

    Event index, participation button, AJAX to be done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = Event.all
+    @events = Event.all.select{|e| current_user.admin_events.exclude?(e)}
   end
 
   # GET /events/1 or /events/1.json
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -3,4 +3,9 @@ class Participation < ApplicationRecord
   belongs_to :user
 
   validates_uniqueness_of :event, :scope => [:user]
+  validate :admin_not_participant
+
+  def admin_not_participant
+    errors.add(:user, "is admin and can't be participant") if (user == self.event.admin)
+  end
 end
