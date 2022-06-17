d0619e47711c40fc3b4a34ab8f487bd6ac472b29
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-06-17 10:43:21 +0200

    LL fix bug emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -30,7 +30,7 @@ Devise.setup do |config|
   config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ActionMailer::Base'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
@@ -132,7 +132,7 @@ Devise.setup do |config|
   # config.send_email_changed_notification = false
 
   # Send a notification email when the user's password is changed.
-  # config.send_password_change_notification = false
+  config.send_password_change_notification = true
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
