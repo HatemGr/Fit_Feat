ddb17d8e4cc801d46d77627c84e0c58ce463f920
HatemGr <hatemgribi@gmail.com>
2022-06-20 23:36:21 +0200

    notif number goes back to zero after checking

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,6 +6,12 @@ class NotificationsController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.notifications.where(viewed: false).update_all(viewed: true)
+    respond_to do |format|
+      format.html { redirect_to user_path(current_user) }
+      format.js {}
+    end
   end
 
   def destroy
