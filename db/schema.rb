06473c277a6e0540a92def5a921aae573040a470
Joff317 <j.freixanet@hotmail.com>
2022-06-21 11:49:16 +0200

    index city with event index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_20_102558) do
+ActiveRecord::Schema.define(version: 2022_06_21_082905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_06_20_102558) do
     t.float "latitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "climbing_block_colors", force: :cascade do |t|
