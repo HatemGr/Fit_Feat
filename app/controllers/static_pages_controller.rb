0fb0b94fc9af8eda85347d4793b90f7251889693
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-06-23 10:51:45 +0200

    LL emails and links on landing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,6 @@ class StaticPagesController < ApplicationController
   end
 
   def create
-    puts"*"*50
-    puts params
     ContactMailer.contact(params[:contact_email], params[:contact_message]).deliver_now
     flash[:success] = "Message a bien été envoyé"
     redirect_to home_path
