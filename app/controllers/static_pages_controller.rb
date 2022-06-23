0409abc4155201b89d7f819b95c5b76a5df9f456
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-06-23 12:19:26 +0200

    LL translate names of sports and update select in event edit form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,7 @@ class StaticPagesController < ApplicationController
 
   def create
     ContactMailer.contact(params[:contact_email], params[:contact_message]).deliver_now
+    ContactMailer.confirmation(params[:contact_email], params[:contact_message]).deliver_now
     flash[:success] = "Message a bien été envoyé"
     redirect_to home_path
   end
