40a08c265b9d3c42d8f785c064612ca37d7cf6a4
foucauld <foucauldbeaumont@yahoo.fr>
2022-06-24 10:24:59 +0200

    adding text in modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -17,6 +17,8 @@ if (modalpop != null) {
 		if (localStorage.getItem("firstSession?") === null) {
 			openModal();
 			localStorage.setItem("firstSession?", true);
+		} else {
+			openModal();
 		}
 	}
 
