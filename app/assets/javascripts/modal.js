9c07cc0109bf1680171f1469f4284d1c4cdaa22a
foucauld <foucauldbeaumont@yahoo.fr>
2022-06-24 09:11:35 +0200

    setting up modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,7 +1,9 @@
 let modalpop = document.getElementById("modalId");
+console.log(modalpop);
 
 if (modalpop != null) {
 	let darkLayer = document.getElementById("layer-dark");
+	console.log(darkLayer);
 
 	function closeModal() {
 		modalpop.style.display = "none";
@@ -9,12 +11,14 @@ if (modalpop != null) {
 	}
 
 	function openModal() {
+		console.log("open modal");
 		darkLayer.style.display = "block";
 		modalpop.style.display = "block";
 	}
 
 	function setLocalStorage() {
 		if (localStorage.getItem("firstSession?") === null) {
+			console.log("null ou pas null");
 			openModal();
 			localStorage.setItem("firstSession?", true);
 		}
@@ -24,8 +28,13 @@ if (modalpop != null) {
 		let userId = document.getElementById("id-user-local").textContent.trim();
 		let urlStorage = `http://localhost:3000/users/${userId}/edit`;
 
+		console.log(userId);
+		console.log(urlStorage);
+
 		if (window.location.href == urlStorage) {
+			console.log("ca va jusque ici");
 			setLocalStorage();
+			console.log("là non");
 		}
 	}, "800");
 }
