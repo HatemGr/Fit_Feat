4ae2e09349c8e6b821005a13df8de3a8fbed2db0
foucauld <foucauldbeaumont@yahoo.fr>
2022-06-24 09:17:48 +0200

    set up modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -26,12 +26,16 @@ if (modalpop != null) {
 
 	setTimeout(() => {
 		let userId = document.getElementById("id-user-local").textContent.trim();
-		let urlStorage = `http://localhost:3000/users/${userId}/edit`;
+		let urlStorageLocal = `http://localhost:3000/users/${userId}/edit`;
+		let urlStorageHeroku = `https://fit-feat.herokuapp.com/users/${userId}/edit`;
 
 		console.log(userId);
 		console.log(urlStorage);
 
-		if (window.location.href == urlStorage) {
+		if (
+			window.location.href == urlStorageLocal ||
+			window.location.href == urlStorageHeroku
+		) {
 			console.log("ca va jusque ici");
 			setLocalStorage();
 			console.log("là non");
