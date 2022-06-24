cfeeb4435cc490709259fffac6287c881dbef2ae
foucauld <foucauldbeaumont@yahoo.fr>
2022-06-24 09:42:56 +0200

    setting up redirecct to user profil

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,12 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  def new
+    if current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
   def create
     User.where(is_admin: true).each do |admin|
       ContactMailer.contact(admin.email, params[:contact_email], params[:contact_message]).deliver_now
